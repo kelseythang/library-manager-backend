9a5e9b6eea7d0b3abad30b8104d5e81e87b512d1
Kelsey Thang <thang.kelsey@gmail.com>
2022-11-20 15:27:52 -0500

    creates model associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
-  
+  belongs_to :author
+  has_one :member, through: :checkout
+  has_many :genres, through: :books_genres
+  has_many :members, through: :books_members
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ActiveRecord::Base
-
+  belongs_to :checkout
+  has_many :books, through: :books_members
 end
