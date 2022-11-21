0274369e756aed2ebfd852cd932d7c65730ec105
Kelsey Thang <thang.kelsey@gmail.com>
2022-11-20 20:18:47 -0500

    installs faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,20 @@ puts 'Seeding Database'
 
 # enter here
 
+# author seeds
+author_list = [
+  ['Stephenie','Meyer'],
+  ['J.K.','Rowling'],
+  ['J.R.R.', 'Tolkien'],
+  ['Frank','Herbert']
+]
+
+author_list.each do |first, last|
+  Author.create(first_name: first, last_name: last)
+end
+
 # genre seeds
-genre_list = ['Fantasy', 'Romance']
+genre_list = ['Fantasy', 'Romance', 'Science Fiction']
 
 genre_list.each do |type|
   Genre.create( name: type )
@@ -30,22 +42,29 @@ Book.create([
   { title: 'Harry Potter and the Deathly Hallows', is_checked_out: false, isbn: 9780545010221, author_id: 2 },
   { title: 'Fantastic Beasts & Where to Find Them', is_checked_out: false, isbn: 9780751574951, author_id: 2 },
   { title: 'Fantastic Beasts: The Crimes of Grindelwald', is_checked_out: false, isbn: 9780751578287, author_id: 2 },
-  { title: 'Fantastic Beasts: The Secrets of Dumbledore', is_checked_out: false, isbn: 9781338853681, author_id: 2 }
+  { title: 'Fantastic Beasts: The Secrets of Dumbledore', is_checked_out: false, isbn: 9781338853681, author_id: 2 },
+  { title: 'The Hobbit', is_checked_out: false, isbn: 9780547928227, author_id: 3 },
+  { title: 'The Fellowship of the Ring', is_checked_out: false, isbn: 9780547928210, author_id: 3 },
+  { title: 'The Two Towers', is_checked_out: false, isbn: 9780547928203, author_id: 3 },
+  { title: 'The Return of the King', is_checked_out: false, isbn: 9780547928197, author_id: 3 },
+  { title: 'The Silmarillion', is_checked_out: false, isbn: 9780544338012, author_id: 3 },
+  { title: 'Unfinished Tales of Numenor and Middle-Earth', is_checked_out: false, isbn: 9780544337992, author_id: 3 },
+  { title: 'The Children of Hurin', is_checked_out: false, isbn: 9780618894642, author_id: 3 },
+  { title: 'Beren and Luthien', is_checked_out: false, isbn: 9781328791825, author_id: 3 },
+  { title: 'Dune', is_checked_out: false, isbn: 9780441172719, author_id: 4 },
+  { title: 'Dune Messiah', is_checked_out: false, isbn: 9780593098233, author_id: 4 },
+  { title: 'Children of Dune', is_checked_out: false, isbn: 9780593098240, author_id: 4 },
+  { title: 'God Emperor of Dune', is_checked_out: false, isbn: 9780593098257, author_id: 4 },
+  { title: 'Heretics of Dune', is_checked_out: false, isbn: 9780593098264, author_id: 4 },
+  { title: 'Chapterhouse: Dune', is_checked_out: false, isbn: 9780593098271, author_id: 4 },
+  { title: '', is_checked_out: false, isbn: , author_id: 4 },
+  { title: '', is_checked_out: false, isbn: , author_id: 4 },
+  { title: '', is_checked_out: false, isbn: , author_id: 4 },
+  { title: '', is_checked_out: false, isbn: , author_id: 4 },
   #{ title: '', is_checked_out: false, isbn: , author_id: 2 },
 ])
 
-# author seeds
-author_list = [
-  ['Stephenie','Meyer'],
-  ['J.K.','Rowling']
-]
-
-author_list.each do |first, last|
-  Author.create( first_name: first, last_name: last)
-end
-
-# books_genres join table seeds
-
 # books_members join table seeds
+# enter data
 
 puts 'Seeding Complete'
